b0e9591ad50ac90978cc1f84410804ceb6e73a92
Yan Zhu <picturepan2@hotmail.com>
2018-05-21 22:55:08 +0800

    Initial commit 👏

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cleancss = require('gulp-clean-css');
+var csscomb = require('gulp-csscomb');
+var rename = require('gulp-rename');
+var LessPluginAutoPrefix = require('less-plugin-autoprefix');
+
+var autoprefix = new LessPluginAutoPrefix({ browsers: ["last 4 versions", "Explorer >= 10", "Firefox ESR"] });
+
+gulp.task('watch', function() {
+  gulp.watch('./**/*.less', ['build']);
+});
+
+gulp.task('build', function() {
+  gulp.src("markdown.less")
+    .pipe(less({
+      plugins: [autoprefix]
+    }))
+    .pipe(csscomb())
+    .pipe(gulp.dest('./dist'))
+    .pipe(cleancss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['build']);
